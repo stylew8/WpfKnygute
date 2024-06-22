19d1d09f34d465d66dde46604ccc3f7733509ec5
stylew8 <123756745+stylew8@users.noreply.github.com>
2024-06-22 14:36:59 +0300

    Update DbHelper.cs

--- DAL/DbHelper.cs
+++ DAL/DbHelper.cs
@@ -9,7 +9,7 @@ namespace WpfKnygute.DAL
 {
     public class DbHelper
     {
-        private const string ConnString = "Data Source=SQL6031.site4now.net;Initial Catalog=db_aa5307_artursobol666;User Id=db_aa5307_artursobol666_admin;Password=artursobol666";
+        private const string ConnString = "";
         private static SqlConnection _connection;
 
         public static SqlConnection GetConnection()
